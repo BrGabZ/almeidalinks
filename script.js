4386206abc504afeac25fb522cdf101de184f0e2
Gabriel Almeida <gabrielallison2022@gmail.com>
2024-09-18 10:59:09 -0300

    Troca de imagem

--- script.js
+++ script.js
@@ -8,4 +8,19 @@ function toggleMode() {
     html.classList.add('light')
   }*/
   html.classList.toggle("light")
+
+  //pegar a imagem
+  const img = document.querySelector('#profile img')
+  //subistituir imagem
+  if(html.classList.contains("light")){
+    //se tiver light troca a imagem
+  img.setAttribute("src", "./assets/avatar-light.png")
+  } else {
+  //se não estiver light fica img padrão
+  img.setAttribute("src", "./assets/avatar.png")
+  }
+
+
 }
+
+
